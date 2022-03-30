df1e5b8847119641b526203e44d0f73233b57606
beni-pc <hangbenjamin2003@gmail.com>
2022-03-30 19:34:29 +0200

    Szép galéria

--- components/Gallery/Camp.tsx
+++ components/Gallery/Camp.tsx
@@ -17,13 +17,13 @@ function Camp() {
       GetElements();
      
     return ( 
-        <div className="">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] gap-4">
         {
           Images.map((i,e) => {
            return(
                e>10 ?
-                <img src={i} key={i} loading="lazy"/>
-                :<img  src={i}  key={i} loading="eager"/>
+                <img src={i} key={i} loading="lazy" className='block max-w-full relative cursor-pointer max-h-80 rounded-md hover:translate-x-1 hover:translate-y-1 hover:shadow-black hover:shadow-md transition-all'/>
+                :<img  src={i}  key={i} loading="eager" className='block max-w-full relative cursor-pointer max-h-80 rounded-md  hover:translate-x-1 hover:translate-y-1 hover:shadow-black hover:shadow-md transition-all'/>
                );
           })
         }  
@@ -31,9 +31,9 @@ function Camp() {
         { 
         LongImages.map((i,e) => {
             return(
-                e>10 ?
-                 <img  src={i}  key={i} loading="lazy"/>
-                 :<img src={i} key={i} loading="eager"/>
+                e<10 ?
+                 <img className='block max-w-full relative cursor-pointer max-h-80 rounded-md  hover:translate-x-1 hover:translate-y-1 hover:shadow-black hover:shadow-md transition-all' src={i}  key={i} loading="eager" />
+                 :<img className='block max-w-full relative cursor-pointer max-h-80 rounded-md  hover:translate-x-1 hover:translate-y-1 hover:shadow-black hover:shadow-md transition-all' src={i} key={i} loading="lazy" />
                 );
            })
         }
