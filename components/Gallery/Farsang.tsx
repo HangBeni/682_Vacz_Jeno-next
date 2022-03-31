3c928a339de58161e6190b91104df3dee4bc77e4
HangBeni <hangbenjamin2003@gmail.com>
2022-03-31 14:58:14 +0200

    Nagyon nics ,eg a gombos megvalósítás

--- components/Gallery/Farsang.tsx
+++ components/Gallery/Farsang.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import Enums from '../Enums'
 
 function Farsang() {
 
     const Images = new Array()
     const LongImages = new Array()
-  
+    const[show,setShow] = useState(false);
        const GetElements= () => {
         for (let i = 1; i < 19; i++) {
         Images.push(`${Enums.FARSANG_IMGS}${i}.JPG`);
@@ -13,15 +14,14 @@ function Farsang() {
       for (let i = 1; i <= 3; i++) {
         LongImages.push(`${Enums.FARSANG_LONG_IMGS}${i}.JPG`);
       }
+      
     }
         
-        
-      return ( 
-          <div className="m-4">
-            <button onClick={GetElements}>Farsang</button> 
+        GetElements();
+      const Show = () =>{
 
-            <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] gap-4">
-            {
+        if(show)
+        return(
           Images.map((i,e) => {
             return(
                 e>10 ?
@@ -29,7 +29,22 @@ function Farsang() {
                  :<Image width={500} height={300} key={e} src={i} loading="eager" className='block max-w-full relative cursor-pointer max-h-80 rounded-md hover:translate-x-1 hover:translate-y-1 hover:shadow-black hover:shadow-md transition-all'/>
                 );
            })
-        }
+        );
+      }
+
+      const showHandler = () => {
+        setShow(show => !show)
+        Show();
+      }
+      useEffect(()=>{
+        [show]
+      })
+      return ( 
+          <div className="m-4">
+            <button onClick={showHandler}>Farsang</button> 
+
+            <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] gap-4">
+            {Show}
             </div>
 
             <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] gap-4">
