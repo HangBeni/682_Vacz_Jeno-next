9be94dcf51abbe783f4b22608b35c986280e4758
beni-pc <hangbenjamin2003@gmail.com>
2022-04-22 11:46:28 +0200

    Elvetve a magasabb szint

--- pages/Gallery.tsx
+++ pages/Gallery.tsx
@@ -1,13 +1,36 @@
-import Link from "next/link";
+import Camp from "../components/Gallery/Camp"
+import  Farsang  from "../components/Gallery/Farsang"
+import supabase from "../utils/supabase"
+
+export async function getStaticProps() {
+
+  const {data: camp} = await supabase.from('Camp').select('*').order('id')
+  const {data: farsang} = await supabase.from('Farsang').select('*').order('id')
+
+  return {
+    props: {
+      camp,
+      farsang
+    },
+  }
+}
+
+type Image = {  
+    id: number
+    imageScr: string
+    tags: string
+  }
 
 
 
- function Gallery() {
+
+
+ function Gallery({ camp, farsang }: { camp: Image[], farsang:Image[] }) {
  return(
- <div>
-   <Link as='/Gallery/Camp' href='/Gallery/[event]'><a>Táborunk</a></Link>
-   <Link as='/Gallery/Farsang' href='/Gallery/[event]'><a>Farsang</a></Link>   
- </div> 
+ <>
+ <Camp camp={camp}/>
+ <Farsang farsang={farsang}/>
+ </> 
  );
 }
 
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,6 +4,6 @@ import { createClient } from "@supabase/supabase-js";
 
 export default createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpaXVkdWpjdnN5cmd4d2F0cHhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDkwMDIyNTYsImV4cCI6MTk2NDU3ODI1Nn0.yOWK_nzu2oPXaTlsh3ajoW1e_fkg3b6A-9MsbCVhKlY' || ''
   )
   
