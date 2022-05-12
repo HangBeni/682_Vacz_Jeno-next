5eb1183c10b1fa91f490009a310ba6a90cdbd696
beni-pc <hangbenjamin2003@gmail.com>
2022-05-12 18:02:44 +0200

    Csapat oldal törvényei pridik

--- components/index/Navbar.tsx
+++ components/index/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     return (
     <nav className=" sticky z-10 top-0 flex rounded-lg border-b-4 border-r-4 border-l-8 border-[#76b82a] bg-white text-xl">
       <div className="container mx-auto flex w-11/12 flex-wrap justify-between  sm:py-2 md:py-4">
-        <h1 className="flex z-1 text-3xl font-extrabold md:after:content-['_V._J.'] lg:after:content-['_Vácz_Jenő'] ">
+        <h1 className="flex z-1 lg:text-3xl font-extrabold md:after:content-['_V._J.'] lg:after:content-['_Vácz_Jenő'] md:text-2xl sm:text-xl my-auto">
           682{' '}
         </h1>
         <button onClick={menu} className="block z-10 mt-2 mr-0 space-y-[5px] md:opacity-0 opacity-100">
