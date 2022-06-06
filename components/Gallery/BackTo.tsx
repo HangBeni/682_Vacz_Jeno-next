343034b25c1c0e08aec8d49682c8db8a9131b20b
HangBeni <hangbenjamin2003@gmail.com>
2022-06-06 21:39:04 +0200

    Menu ki be a Galérián

--- components/Gallery/BackTo.tsx
+++ components/Gallery/BackTo.tsx
@@ -9,12 +9,12 @@ function Back() {
         <div
         className="fixed bottom-2 right-2 m-1 z-30 cursor-pointer">
             <ScrollToTop 
-            className="bg-[#76b82a] rounded-3xl p-1"
+            className="rounded-3xl p-1"
             component={ 
                 <Image
                 src={'https://ik.imagekit.io/HangBeni/Tabor/156110-removebg-preview_2-hsyCcxK.png?ik-sdk-version=javascript-1.4.3&updatedAt=1654337895856'}
                     layout='fixed'
-                    width={'50px'}
+                    width={'75px'}
                     height={'125px'}
                 />}
             />
--- components/Gallery/Menu.tsx
+++ components/Gallery/Menu.tsx
@@ -1,15 +1,39 @@
+import { motion } from "framer-motion";
 import Link  from "next/link";
+import { useState } from "react";
+import ScrollToTop from "react-scroll-to-top";
+
+
+
+
+
 
 function EventHandler() {
+
+  const [direction_Gallery, setDirection] = useState(false);
+
+  const variants_Gallery = {
+    up:{y:"-200%", transition:{duration:1}},
+    down:{y:0,transition:{duration:1}}
+  }
+
+  const variants_arrow_Gallery = {
+    down:{ transition:{duration:1}, rotate: 180,},
+    up:{transition:{duration:1}, rotate: 0,}
+  }
     return ( 
-        <div>
+      <>
+      <motion.div 
+      variants={variants_Gallery} initial="up" animate={direction_Gallery ? "down" : "up"}
+      className="w-11/12 mx-auto rounded-b-xl bg-[#009a93]">
+        <div className="flex justify-between w-1/2 mx-auto">
             <Link
           href={{
             pathname: '/Gallery/[event]',
             query: { event: 'Camp' },
           }}
         >
-            Táborunk
+              <a className="lg:text-2xl text-lg p-4 mx-1 font-semibold">Táborunk</a>
             </Link>
             <Link
           href={{
@@ -17,10 +41,19 @@ function EventHandler() {
             query: { event: 'Farsang' },
           }}
         >
-                <a>Farsang</a>
+              <a className="lg:text-2xl text-lg p-4 mx-1 font-semibold">Farsang</a>
             </Link>
         </div>
+    
+      </motion.div>
+      <motion.span
+       variants={variants_arrow_Gallery} initial="up" animate={direction_Gallery ? "down" : "up"}
+      onClick={() => setDirection(direction_Gallery=> !direction_Gallery)}
+      className="absolute font-bold text-xl right-10 top-14 bg-[#009a93] p-2 m-1 rounded-full translate-y-0 z-20">&#8593;</motion.span>
+      </>
      );
 }
 
 export default EventHandler;
+
+
