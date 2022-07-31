1412c893ed09c3bd2a73f316713030de79228cba
HangBeni <hangbenjamin2003@gmail.com>
2022-07-31 23:59:52 +0200

    Nav, Contact, Donation section

---
+++ components/index/Donation.tsx
@@ -0,0 +1,16 @@
+function Donation() {
+  return (
+    <div className="block h-full bg-[#76b82a]">
+      <h1 className="text-3xl font-extrabold">Támogatási lehetőség</h1>
+      <p>
+        Amennyiben támogatni szeretné csapatunkat, azt ezen a számlára utalva
+        teheti meg () a Hosszúhetényi Cserkész Baráti kör számára. Ezekkel a
+        támogatásokkal felszereléseket vásárolunk és a programjainkat
+        finanszírozzuk, amiket a gyerekek részére csinálunk. Az adományozás
+        teljesen önkéntes.
+      </p>
+    </div>
+  )
+}
+
+export default Donation
