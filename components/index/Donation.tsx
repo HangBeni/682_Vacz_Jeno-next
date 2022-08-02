d852a82971a41d0df0be28c9b32fd921cdde74dd
HangBeni <hangbenjamin2003@gmail.com>
2022-08-02 14:22:25 +0200

    Designing

--- components/index/Donation.tsx
+++ components/index/Donation.tsx
@@ -1,15 +1,21 @@
+import { motion } from "framer-motion"
+
 function Donation() {
   return (
-    <div className="block h-full pb-auto bg-trendy_green text-justify">
-      <h1 className="text-3xl font-extrabold pt-4">Támogatási lehetőség</h1>
-      <p className="text-lg mt-5 px-2 ">
+    <motion.div
+    initial={{opacity: 0, x:'40%'}}
+    whileInView={{opacity:1, x:0}}
+    transition={{duration: 0.5}}
+    className="block h-full pb-10 bg-trendy_green text-justify">
+      <h1 className="text-3xl font-extrabold pt-4 text-center sm:text-left">Támogatási lehetőség</h1>
+      <p className="text-lg mt-5 px-5 ">
         Amennyiben támogatni szeretné csapatunkat, azt ezen a számlára utalva
         teheti meg: <strong>11773315-02097341</strong>, a <strong> Hosszúhetényi Cserkész Barátikör</strong> számára. Ezekkel a
         támogatásokkal felszereléseket vásárolunk és a programjainkat
         finanszírozzuk, amiket a gyerekek részére csinálunk. Az adományozás
         teljesen önkéntes, mindent hálával fogadunk.
       </p>
-    </div>
+    </motion.div>
   )
 }
 
