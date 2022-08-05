68ffb3537afd381ebc4341be7c48ed22b8b95558
HangBeni <hangbenjamin2003@gmail.com>
2022-08-05 13:08:01 +0200

    Next update, and diff home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import About from '../components/index/About'
 import supabase from '../utils/supabase';
 import TeamMates from '../components/Team/TeamMates';
 import Laws from '../components/Team/Laws';
+import Image from 'next/future/image';
 
 
 
@@ -22,12 +23,28 @@ type Image = {
 
 
 function Home({data}:{data:Image[]}) {
+  
+  var teamPics:any;
+  data.forEach((image) => {
+   if(image?.tags?.includes('Csapatunk nyáritáborában'))
+    teamPics = image?.imageScr
+  });
+
+  
   return (
-    <div>
+    <>
+    <div className='relative w-full sm:h-72 md:h-[25rem] lg:h-[30rem]'>
+        <div className='absolute block w-5/6 h-full left-0 right-0 mx-auto '>
+          <Image 
+          src={teamPics}
+          sizes='100vw' fill priority
+          />
+        </div>
+      </div>
       <About data={data}/>
       <Laws/>
       <TeamMates/>
-    </div>
+    </>
   );
 }
 export default Home
