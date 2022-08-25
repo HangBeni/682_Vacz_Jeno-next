19991d4652442d26eccfc24290f9f36e20385ed9
HangBeni <hangbenjamin2003@gmail.com>
2022-08-25 09:43:46 +0200

    Updates

--- components/Curriculum/Nepdalok/Dalok.tsx
+++ components/Curriculum/Nepdalok/Dalok.tsx
@@ -2,16 +2,16 @@ import { Dal } from "../../../pages/Curriculum/Nepdal";
 
 function Dalok({dalok}: {dalok: Dal[]}) {
     return ( 
-        <div className="grid grid-cols-1 md:grid-cols-[repeat(auto-fit,_minmax(300px,1fr))]">
+        <div className="grid place-items-center grid-cols-1 md:grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] mt-10">
             {dalok.map((dal) => {
                 return (
                     <div>
-                        <video className="rounded-xl" width={300} controls playsInline >
+                        <video className="rounded-xl mx-auto" width={300} controls playsInline >
                             <source src={dal.Video} type="video/mp4"/>
                         </video>
-                        <h2 className="text-xl">{dal.Name}</h2>
+                        <h2 className="text-xl md:text-2xl">{dal.Name}</h2>
                         <p>{dal.Lyrics.split('$').map((versszak) => {
-                            return <div className="m-2 my-4">{versszak.split('|').map((sor) => {
+                            return <div className="m-2 my-4 text-lg md:text-xl">{versszak.split('|').map((sor) => {
                                 return<span className="block">{sor}</span>
                             })}</div>
                         })}</p>
