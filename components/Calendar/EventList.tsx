477675fc4e2fa7196f25e166eee2476b1e1c3202
HangBeni <hangbenjamin2003@gmail.com>
2022-09-11 19:39:32 +0200

    Design update for event card

--- components/Calendar/EventList.tsx
+++ components/Calendar/EventList.tsx
@@ -2,22 +2,23 @@ import {Event} from "../../utils/Types"
 import Image from "next/future/image";
 function EventList({events}: {events: Event[]}) {
     return ( 
-        <div className="grid grid-cols-[repeat(auto-fit,_minmax(450px,1fr))] place-items-center
-                        mt-10 text-font">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] gap-3 mt-10 text-font">
             {events.map((event) => {
                 return(
-                    <div className="w-[35ch] rounded-lg m-5 border-türkisz border-solid border-2">
+                    <div className="lg:flex w-[35ch] md:w-5/12 lg:w-2/4 max-w-lg lg:max-w-2xl bg-bg rounded-lg m-5 border-türkisz border-solid border-2">
                         <Image
                         src={event.Img}
                         width={300}
                         height={250}
-                        className='rounded-lg'
+                        className='rounded-xl w-full max-w-lg lg:max-w-xl'
                         />
-                        <div className="p-5 bg-[#44de444d]">
-                            <h2 className="after:absolute after:h-[3px] after:left-0 after:bottom-[-2px] after:w-full after:bg-türkisz
-                                            relative text-2xl">{event.ProgramNeve}</h2>
-                            <p className="text-lg leading-7 mt-2">{event.Részletek}</p>
-                            <p className="font-bold">{event.Program_Kezdete.toString()} <strong>-</strong> {event.Program_Vege.toString()}</p>
+                        <div className="p-3 bg-[#44de444d] ">
+                            <h2 className="after:absolute after:h-[3px] after:left-0 after:bottom-[-2px] after:w-full after:bg-türkisz after:scale-x-0 after:origin-left hover:after:scale-x-100 after:duration-300
+                                            [@media(hover:none)]:after:scale-x-100
+                                            relative text-2xl lg:text-3xl">{event.ProgramNeve}</h2>
+                            <p className="text-lg lg:text-xl leading-7 mt-2">{event.Részletek}</p>
+                            <p className="text-xl lg:text-2xl font-bold">Kezdete: <span className="relative after:absolute after:h-[3px] after:left-0 after:bottom-[-2px] after:w-full after:bg-regos after:scale-x-0 after:origin-left hover:after:scale-x-100 after:duration-300 [@media(hover:none)]:after:scale-x-100">{event.Program_Kezdete}</span></p>
+                            <p className="text-xl lg:text-2xl font-bold">Vége: <span className="relative after:absolute after:h-[3px] after:left-0 after:bottom-[-2px] after:w-full after:bg-regos after:scale-x-0 after:origin-left hover:after:scale-x-100 after:duration-300 [@media(hover:none)]:after:scale-x-100">{event.Program_Vege}</span></p>
                         </div>
                     </div>
                 )
