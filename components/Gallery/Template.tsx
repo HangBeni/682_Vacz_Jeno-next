b4f74e9613466e8d549561c57f639826497fd9ba
HangBeni <hangbenjamin2003@gmail.com>
2022-11-30 17:39:31 +0100

    SCR --> Src

--- components/Gallery/Template.tsx
+++ components/Gallery/Template.tsx
@@ -11,15 +11,15 @@ function Template({ images }: { images: image[] }) {
 
   const back = () => {
     images.map((img) => {
-      if (images.findIndex(img => img.imageScr == tempimg) == img.id) 
-        setimg(img.imageScr)
+      if (images.findIndex(img => img.imageSrc == tempimg) == img.id) 
+        setimg(img.imageSrc)
     })
   }
 
   const forward = () => {
     images.map((img) => {
-      if (images.findIndex(img => img.imageScr == tempimg)+2 == img.id) 
-        setimg(img.imageScr)
+      if (images.findIndex(img => img.imageSrc == tempimg)+2 == img.id) 
+        setimg(img.imageSrc)
     })
   }
 
@@ -57,20 +57,20 @@ function Template({ images }: { images: image[] }) {
 
         <div  className="pt-4 grid grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] place-items-center gap-3 ">
               {images?.map((image:any) => {
-              if(image.imageScr.includes('long'))
+              if(image.imageSrc.includes('long'))
               return (          
                   
                   <Image
                   width={300}
                   height={200}
                   quality={80}
-                  src={image.imageScr}
+                  src={image.imageSrc}
                   key={image.id}
                   title={image.tags}
                   alt={image.tags}
                   className=' border-border_color_primary border
                   relative block h-full row-span-2 max-h-full max-w-full cursor-pointer rounded-md transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:shadow-black'
-                  onClick={() => (setimg(image.imageScr), setModal(true))}
+                  onClick={() => (setimg(image.imageSrc), setModal(true))}
                   />
                   )
                   else
@@ -80,13 +80,13 @@ function Template({ images }: { images: image[] }) {
                       width={300}
                       height={300}
                       quality={80}
-                      src={image.imageScr}
+                      src={image.imageSrc}
                       key={image.id}
                       title={image.tags}
                       alt={image.tags}
                       className=' border-border_color_secondary border
                       block max-h-full max-w-full cursor-pointer rounded-md transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:shadow-black'
-                      onClick={() => (setimg(image.imageScr), setModal(true))}
+                      onClick={() => (setimg(image.imageSrc), setModal(true))}
                       />
                   
                       )
