107992b598fd2513eb2d65c44ed76752313ef29a
HangBeni <85244745+HangBeni@users.noreply.github.com>
2023-01-05 18:37:27 +0000

    Webm format fix

--- components/Curriculum/Nepdalok/Dalok.tsx
+++ components/Curriculum/Nepdalok/Dalok.tsx
@@ -10,21 +10,21 @@ function Dalok({dalok}: {dalok: Dal[]}) {
     
     
     return ( 
-        <div className="grid grid-cols-1 md:grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] mt-10 gap-5">
-            {dalok.map((dal) => {
-                
+        <div className="grid grid-cols-1 md:grid-cols-[repeat(auto-fit,_minmax(350px,1fr))] mt-10 gap-5">
+            {dalok.map((dal) => { 
                 return (
-                    <div className="bg-bg_secondary text-font rounded-2xl h-auto">
+                    <div className="bg-bg_secondary text-font rounded-2xl h-min">
                         <video className="rounded-xl mx-auto w-full max-h-60" width={300} controls playsInline >
-                            <source src={dal.Video} type="video/mp4"/>
+                            <source src={dal.Video} type="video/mp4" />
+                            <source src={dal.Video} type="video/webm" />
                         </video>
                         <h2 className="text-xl md:text-2xl">{dal.Name}</h2>
-                        <div
-                        
-                        className="">
+                        <div>
                             <p>{dal.Lyrics.split('$').map((versszak) => {
-                                return <div className="m-2 my-4 text-lg md:text-xl">{versszak.split('|').map((sor) => {
-                                    return<span className="block">{sor}</span>
+                                return <div className="m-2 my-4 text-lg md:text-xl">
+                                    {
+                                        versszak.split('|').map((sor) => {
+                                        return<span className="block">{sor}</span>
                                 })}</div>
                             })}</p>
                         </div>
