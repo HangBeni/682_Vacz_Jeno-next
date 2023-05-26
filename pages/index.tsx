571d7792bdd48fa98327e51e3bcec54ce6c426ad
HangBeni <hangbenjamin2003@gmail.com>
2023-05-26 20:01:40 +0200

    New method to filter team pics

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,48 +6,41 @@ import Image from 'next/image';
 import {image} from '../utils/Types'
 
 
-export async function getStaticProps() {
- const {data:data} = await supabase.from('2021_Camp').select('*').order('id')
-  return{
-      props:{
-          data,
-      }
-  }
-}
-
-
-
-
 function Home({data}:{data:image[]}) {
   
-  var teamPics:image;
-  var count = 0;
-  data.forEach((image) => {
-   if(image?.tags?.includes('Csapatunk nyáritáborában') && count == 0)
-    {
-      teamPics = image;
-      count++;
-    }
-    });
+  const teamPics:image[] = data!.filter((image) => image.tags === 'Csapatunk nyáritáborában');  
 
   
   return (
     <div className='text-font bg-bg'>
     <div className='  relative w-full sm:h-72 md:h-[25rem] lg:h-[30rem] h-full'>
         <div className='absolute block w-5/6 h-full left-0 right-0 mx-auto '>
-          <Image 
-          src={teamPics!.imageSrc}
-          sizes='100vw' priority
+        <Image 
+          src={teamPics[0]!.imageSrc}
+          sizes={'100dvw'} fill
+          priority
           className='object-contain'
-          title={teamPics!.tags}
-          alt={'Képek a csapatról'}
+          title={teamPics[0]?.tags}
+          alt={'Képek a csapatunkról'}
           />
         </div>
       </div>
-      <About data={data}/>
+      <About data={teamPics.slice(1,teamPics.length)}/>
       <Laws/>
       <TeamMates/>
     </div>
   )
 }
+
+export async function getStaticProps() {
+
+  const {data} = await supabase!.from('2021_Camp').select('*').order('id');
+ 
+   return{
+       props:{
+           data,
+       }
+   }
+ }
+
 export default Home
