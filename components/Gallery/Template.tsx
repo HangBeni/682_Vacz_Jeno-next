fecf85300a693e147ddaf4839c1c5809dce1d8a2
HangBeni <hangbenjamin2003@gmail.com>
2023-05-30 22:07:01 +0200

    Image layout upgrade (rowspan)

--- components/Gallery/Template.tsx
+++ components/Gallery/Template.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import Image from 'next/image'
 import { image } from '../../utils/Types'
 import Loading from './Loading'
@@ -9,17 +9,54 @@ function Template({ images }: { images: image[] }) {
   const [tempimg, setimg] = useState("")
   const [actuall, setActuall] = useState(0)
   const [modal, setModal] = useState(false)
- 
+  
+  
 
+  const Card = (image:image, index:number) => {
+    if(image.imageSrc.includes("long"))
+                    return(          
+                      <Image
+                      width={340}
+                      height={380}
+                      quality={80}
+                      loading='lazy'
+                      src={image.imageSrc}
+                      key= {index}
+                      title={image.tags}
+                      alt={image.tags}
+                      className='border-border_color_primary border aspect-[3/4]
+                      row-span-2 h-auto max-w-full cursor-pointer rounded-md transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:shadow-black'
+                      onClick={() => {
+                        setModal(true)
+                        setimg(image.imageSrc)
+                        setActuall(index)
+                      }}/>);
+                    else
+                      return(
+                          <Image
+                          width={340}
+                          height={190}
+                          quality={80}
+                          loading='lazy'
+                          src={image.imageSrc}
+                          key={index}
+                          title={image.tags}
+                          alt={image.tags}
+                          className='border-border_color_secondary border row-span-[1/2]
+                          max-h-full  max-w-full cursor-pointer rounded-md transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:shadow-black'
+                          onClick={() => {
+                            setModal(true)
+                            setimg(image.imageSrc)
+                            setActuall(index)
+                            }}
+                            />)
+  }
 
-   let imageDictionary = new Map<number, string>;
-   images.map(img => {
-    imageDictionary.set(img.timeLine,img.imageSrc)
-   })
+   
 
   const back = () => {
     setimg('')
-    setimg(imageDictionary.get(images.find((img) => img.timeLine == actuall - 1)?.timeLine || 0) || "")
+    setimg(imageDictionary.get(images.find((img) => img.timeLine === actuall - 1)?.timeLine || 0) || "")
     if(actuall != 1)
       setActuall(actuall - 1)
     else
@@ -48,9 +85,9 @@ function Template({ images }: { images: image[] }) {
        onClick={back}
        >&#60;</span>
 
+<Suspense fallback={<Loading/>}>
       {tempimg && 
-      <div className="mx-auto box-border block h-max w-max p-3">
-        <Suspense fallback={<Loading/>}>
+      <div className="relativ mx-auto box-border block h-max w-max p-3">
           {tempimg.includes('long') ?
        <Image
        className='block h-full mb-2 w-auto object-contain' 
@@ -66,8 +103,8 @@ function Template({ images }: { images: image[] }) {
         src={tempimg}
         alt='Aktuálisan kiemelt kép'
        />}
-       </Suspense>
        </div>}
+       </Suspense>
 
        
         <span
@@ -82,50 +119,17 @@ function Template({ images }: { images: image[] }) {
         
       </div>
 
-      <div className='mx-1 '>
+      <div className='mx-1'>
 
       <section 
-        className="pt-4 grid grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] place-items-center gap-3 ">
-              {images?.map((image:image) => {
-                if(image.imageSrc.includes('long'))
-                return (          
-                    <Image
-                    width={300}
-                    height={240}
-                    quality={80}
-                    loading='lazy'
-                    src={image.imageSrc}
-                    key={image.id}
-                    title={image.tags}
-                    alt={image.tags}
-                    className='border-border_color_primary border
-                    block h-full row-span-2 max-h-[95%] max-w-full cursor-pointer rounded-md transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:shadow-black'
-                    onClick={() => {
-                      setModal(true)
-                      setimg(imageDictionary.get(image.timeLine) || "")
-                      setActuall(image.timeLine)
-                    }}/>
-                  )
-                  else
-                    return (
-                        <Image
-                        width={300}
-                        height={280}
-                        quality={80}
-                        loading='lazy'
-                        src={image.imageSrc}
-                        key={image.id}
-                        title={image.tags}
-                        alt={image.tags}
-                        className=' border-border_color_secondary border
-                        block max-h-full max-w-full cursor-pointer rounded-md transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:shadow-black'
-                        onClick={() => {
-                          setimg(imageDictionary.get(image.timeLine) || "")
-                          setActuall(image.timeLine)
-                          setModal(true)}}
-                          />
-                        )
-                        
+        className="pt-4 grid relative grid-cols-[repeat(auto-fit,_minmax(300px,1fr))] place-items-center gap-3"
+        >
+              {images?.map((image:image, i:number) => {
+                return(
+                  <>
+                  {Card(image, i)}
+                  </>
+              );
               })
             }
           </section>
@@ -133,6 +137,11 @@ function Template({ images }: { images: image[] }) {
         </div>
     </div>
   )
+
+  
+
 }
 
 export default Template
+
+
