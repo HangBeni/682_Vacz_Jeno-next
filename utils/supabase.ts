55169311d80270ca3b7da76d9bb3495f9505d0ee
HangBeni <hangbenjamin2003@gmail.com>
2024-05-14 19:14:27 +0200

    dk

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 
 export default createClient(
-   'https://viiudujcvsyrgxwatpxm.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpaXVkdWpjdnN5cmd4d2F0cHhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDkwMDIyNTYsImV4cCI6MTk2NDU3ODI1Nn0.yOWK_nzu2oPXaTlsh3ajoW1e_fkg3b6A-9MsbCVhKlY'
+  process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://viiudujcvsyrgxwatpxm.supabase.co',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpaXVkdWpjdnN5cmd4d2F0cHhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDkwMDIyNTYsImV4cCI6MTk2NDU3ODI1Nn0.yOWK_nzu2oPXaTlsh3ajoW1e_fkg3b6A-9MsbCVhKlY'
   );
   
