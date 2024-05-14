abd24fa3302ebc0fd852a471b595a9249ecdfcbc
HangBeni <85244745+HangBeni@users.noreply.github.com>
2024-05-14 15:43:00 +0200

    Update events.ts

--- utils/events.ts
+++ utils/events.ts
@@ -5,7 +5,7 @@ export const events = [
     {name:'2022 Májusi Portya' ,db_name:'2022_May_Portya'},
     {name:'Kerületi Nagytábor' ,db_name:'2022_KNT'},
     {name:'2022 Téli Portya' ,db_name:'2022_Winter'},
-    {name:'2022 Regös Napok' ,db_name:'Regos_Napok'},
+    {name:'2022 Regös Napok' ,db_name:'2022_Regos_Napok'},
     {name:'2023 Locsolóbál' ,db_name:'2023_Locsolobal'},
     {name:'2023 Márciusi Portya' ,db_name:'2023_March_Portya'},
     {name:'2023-as Farsang' ,db_name:'2023_Farsang'}
